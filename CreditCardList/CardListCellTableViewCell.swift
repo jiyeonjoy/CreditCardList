b055c212496d4e5a4b388361fecc02c9aa7ed3fb
jiyeonchoi <choi02647@naver.com>
2022-11-12 18:23:14 +0900

    added CreditCardCell

---
+++ CreditCardList/CardListCellTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CardListCellTableViewCell.swift
+//  CreditCardList
+//
+//  Created by Jiyeon Choi on 2022/11/12.
+//
+
+import UIKit
+
+class CardListCellTableViewCell: UITableViewCell {
+    @IBOutlet weak var cardImageView: UIImageView!
+    @IBOutlet weak var rankLabel: UILabel!
+    @IBOutlet weak var promotionLabel: UILabel!
+    @IBOutlet weak var cardNameLabel: UILabel!
+}
