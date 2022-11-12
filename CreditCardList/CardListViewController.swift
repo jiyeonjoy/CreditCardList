9e8219050531ee692a4d1682f8afb1d3371a8912
jiyeonchoi <choi02647@naver.com>
2022-11-12 18:26:52 +0900

    tableView cell 연결

--- CreditCardList/CardListViewController.swift
+++ CreditCardList/CardListViewController.swift
@@ -9,4 +9,30 @@ import UIKit
 
 class CardListViewController: UITableViewController {
     
+    var creditCardList: [CreditCard] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //UITableView
+        let nibName = UINib(nibName: "CardListCellTableViewCell", bundle: nil)
+        tableView.register(nibName, forCellReuseIdentifier: "CardListCellTableViewCell")
+    }
+}
+
+extension CardListViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return creditCardList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "CardListCellTableViewCell", for: indexPath) as? CardListCellTableViewCell else { return UITableViewCell() }
+        
+        let imageURL = URL(string: creditCardList[indexPath.row].cardImageURL)
+        cell.rankLabel.text = "\(creditCardList[indexPath.row].rank)위"
+        cell.promotionLabel.text = "\(creditCardList[indexPath.row].promotionDetail.amount)만원 증정"
+        cell.cardNameLabel.text = creditCardList[indexPath.row].name
+        
+        return cell
+    }
 }
