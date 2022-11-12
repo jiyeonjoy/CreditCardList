d78adc2c2a267dae84fef48bb03b7d6451cc3766
jiyeonchoi <choi02647@naver.com>
2022-11-12 18:41:33 +0900

    added card detail view

---
+++ CreditCardList/CardDetailViewController.swift
@@ -0,0 +1,12 @@
+//
+//  CardDetailViewController.swift
+//  CreditCardList
+//
+//  Created by Jiyeon Choi on 2022/11/12.
+//
+
+import UIKit
+
+class CardDetailViewController: UIViewController {
+    
+}
--- CreditCardList/CardListViewController.swift
+++ CreditCardList/CardListViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Kingfisher
 
 class CardListViewController: UITableViewController {
     
@@ -29,10 +30,15 @@ extension CardListViewController {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "CardListCellTableViewCell", for: indexPath) as? CardListCellTableViewCell else { return UITableViewCell() }
         
         let imageURL = URL(string: creditCardList[indexPath.row].cardImageURL)
+        cell.cardImageView.kf.setImage(with: imageURL)
         cell.rankLabel.text = "\(creditCardList[indexPath.row].rank)위"
         cell.promotionLabel.text = "\(creditCardList[indexPath.row].promotionDetail.amount)만원 증정"
         cell.cardNameLabel.text = creditCardList[indexPath.row].name
         
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
     }
 }
